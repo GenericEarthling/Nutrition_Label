175f5257fef40c9209888e901a73d79e599b5ee0
GenericEarthling <sharon.tender@icloud.com>
2018-10-25 20:51:49 -0400

    added UML Diagram (wip) and updated TODO notes

--- src/utilities/Notes.java
+++ src/utilities/Notes.java
@@ -5,21 +5,37 @@ public class Notes {
 /*...........  Project status and areas that need addressing  ..................
     
 MainWindow    
-Need to check for empty string ingredient when inputing amount (last entry above button)    
-when deleting ingredient from table, 
-    it displays NaN, then tries to add/subtract to subtotals
-    the ingredient still shows up in Table #2 (Values in the actual label are correct)
-        change MainWindow/Edit Button   &    MainWindow/Delete Button
-The Ingredient running totals need to have precision set to one decimal   
-When searching for an ingredient, I want to be able to hit return to get results    
+    Need to check for empty string ingredient when inputing amount (last entry above button)    
+    Edit/Delete Buttons
+        When deleting ingredient from table it displays NaN, then tries to add/subtract to subtotals
+        The ingredient still shows up in Table #2 (Values in the actual label are correct)
+            This should be fixed when adding the new class for saving list of ingredients
+    The Ingredient running totals need to have precision set to one decimal   
+    When searching for an ingredient, I want to be able to hit return to get results 
+    Search Button: if blank when clicking button, 2 erros come up:
+        1) please input an ingredient to search
+        2) Ingredient not found
+        
+See UML-Diagram-Form-fall 2018.vsdx for new classes and current class revisions 
+    Need to delete the Database references for now
+    move GramEquivalent to Calculateor Class
+    
+Calculator class
+    all members should be final
+    methods should be static
     
 NutritionLabel
-Need to be able to print label and ingredients    
+    Need to be able to print label and ingredients    
+    Add Buttons: Cancel, Save Recipe, Print
+    Get rid of table and replace with a list of ingredients
+        see Example file in CodeTinkering (below)
+    Right justify totals in the Label 
     
 DATABASE info
     make database connection class
     example files: 
         CodeTinkering -> database -> SimpleJdbc.java
+        CodeTinkering -> collections -> iterating over recipe w/ingred using a HashMap
         WebTinkering -> servlets -> SimpleRegistration.java
     
     
